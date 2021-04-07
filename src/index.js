8e453d91a32e6f1862548f1b5b65f897b6fcc671
luke beach <lukebeach90@gmail.com>
2021-04-07 18:40:30 +0100

    redux built

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider, connect} from 'react-redux';
-import{createStore} from 'redux';
-import './index.css';
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
-import searchRobots from './reducers'
-import 'tachyons';
-
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+import "./index.css"
+import App from "./containers/App"
+import reportWebVitals from "./reportWebVitals"
+import { searchRobots } from "./reducers"
+import "tachyons"
 
 const store = createStore(searchRobots)
 
-ReactDOM.render(<App store={store} />, document.getElementById('root')
-);
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
